ef475a3bf99a547c831da06664bd2680abe6b798
akhil chowdary <akhilchowdary.m007@gmail.com>
2020-07-28 17:13:50 +0530

    second commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,8 +3,7 @@ from myapp import views
 app_name="myapp"
 urlpatterns = [
     path('',views.index,name='index'),
-    path('home',views.home,name="home"),
-    path('fact/<n>',views.fact,name='fact'),
+    
     path('child/',views.child,name='child')
 
 
--- myapp/views.py
+++ myapp/views.py
@@ -4,10 +4,6 @@ from math import factorial
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Welcome To Views Of An APP</h1>")
-def home(request):
-    return render(request,"myapp/home.html",{'name':"Akhil"})
-def fact(request,n):
-    n=int(n)
-    return HttpResponse("<h3>Factorial is {}</h3>".format(factorial(n)))
+
 def child(request):
     return render(request,"child.html")
